5d2c77d3a053511f0fef5588e141f15a366bb6e9
its-m4npreet <m4npreet006@gmail.com>
2026-06-30 21:37:46 +0530

    Refactor import paths for Prisma client in prisma.ts to use relative paths instead of absolute paths.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client"
+import { PrismaClient } from "../app/generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
@@ -9,4 +9,4 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
-export * from "@/app/generated/prisma/client"
+export * from "../app/generated/prisma/client"
